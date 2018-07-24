127ab7d158ab61aa76a6c6493ceca1cdd4963b50
heverton155 <heverton155@users.noreply.github.com>
2018-07-23 22:55:54 -0300

    23_07_2018
    Commit 23_07_2018

--- test/app/src/main/java/com/example/heverton/test/InitMainActivity.java
+++ test/app/src/main/java/com/example/heverton/test/InitMainActivity.java
@@ -2,8 +2,19 @@ package com.example.heverton.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class InitMainActivity extends AppCompatActivity {
+
+    private ListView lista_venceram;
+    private ImageButton Bt_Acougue;
+    private ImageButton Bt_Bebidas;
+    private ImageButton Bt_Frios;
+    private ImageButton Bt_Higiene;
+    private ImageButton Bt_Hortifruti;
+    private ImageButton Bt_Mercearia;
+    private ImageButton Bt_Padaria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
